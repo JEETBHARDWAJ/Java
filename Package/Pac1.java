b342869de52d06ef73405748aeb4e809b0065c32
JEETBHADWAJ <bhardwajjeet408@gmail.com>
2024-01-11 19:56:01 +0530

    dasf

---
+++ Package/Pac1.java
@@ -0,0 +1,14 @@
+package package1 ;
+public class Pac1 {
+    private int a;
+    private int b;
+    
+    public void setValue(int x, int y){
+        a = x;
+        b = y;
+    }
+    public void display(){
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ Package/Pac2.java
@@ -0,0 +1,11 @@
+package Package2;
+import package1.*;
+class Pac2 {
+    public static void main(String []args){
+        Pac1 s1 = new Pac1();
+        s1.setValue(5,3);
+        s1.display();
+
+    }
+    
+}
