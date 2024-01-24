92d92560beb52964fc843b0d6ce2e8aac649286f
JEETBHADWAJ <bhardwajjeet408@gmail.com>
2024-01-24 23:00:10 +0530

    XCVBNM

--- Hariom.java
+++ Hariom.java
@@ -1,8 +1,10 @@
 class Jeet{
-    static 
+    static int a =10 ;
 
 }
 public class Hariom {
-    
-
+    public static void main(String args[]){
+        System.out.println(Jeet.a);
+        System.out.println(a);
+    }
 }
