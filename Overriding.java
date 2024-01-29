aea760cbb635062ac0feb41ce1a58ccc896705ff
JEETBHADWAJ <bhardwajjeet408@gmail.com>
2024-01-29 23:55:46 +0530

    cvhg

--- Overriding.java
+++ Overriding.java
@@ -4,17 +4,22 @@ void display(){
 }
 }
 
-class V extands S{
-    void fun(int a , int b){
-        System.out.println("calss V");
-        int sum = a+b;
+class V extends S{
+    @Override
+    void display(){
+
+        System.out.println("calss V ");
+
     }
+
 
 }
 public class Overriding {
 
     public static void main(String args[])
     {
+    S s1 = new S();
+    s1.display();
 
         
     }
