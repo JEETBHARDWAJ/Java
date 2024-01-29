0ab35aa2f279564e4c20a233db9b7b4667b811ea
JEETBHADWAJ <bhardwajjeet408@gmail.com>
2024-01-29 21:06:34 +0530

    neww

---
+++ Function_Overloding.java
@@ -0,0 +1,19 @@
+class NewFun{
+    void fun1(int a, int b)
+    {
+        int sum = a+b;
+    }
+    void fun1(int a , int b, int c)
+    {
+        int sum = a+b+c;
+        System.out.println(sum);
+        System.out.println(sum);
+    }
+}
+public class Function_Overloding {
+    public static void main(String args[])
+    {
+
+    }
+    
+}
