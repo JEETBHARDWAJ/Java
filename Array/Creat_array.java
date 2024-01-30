d2e6bb514bd91fb2c3f5e09db925ba6e533f6c11
JEETBHADWAJ <bhardwajjeet408@gmail.com>
2024-01-31 00:19:16 +0530

    nwe

---
+++ Array/Creat_array.java
@@ -0,0 +1,22 @@
+public class Creat_array {
+
+    public static void main(String[] args) {
+        int ary[] = new int[11]; //  Alocation space for the array 
+        ary[0] = 0;
+        ary[1] = 1;
+        ary[2] = 2;
+        ary[3] = 3;
+        ary[4] = 4;
+        ary[5] = 5;
+        ary[6] = 6;
+        ary[7] = 7;
+        ary[8] = 8;
+        ary[9] = 9;
+        ary[10] = 10;
+        for(int i=0; i<11 ;i++)
+        {
+            System.out.println(ary[i]);
+        }
+    }
+    
+}
---
+++ Array/Loop_for creationg _array.java
@@ -0,0 +1,11 @@
+package Array;
+
+/**
+ * Loop_for creationg _array
+ */
+public class Loop_for creationg _array {
+
+    
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+}
