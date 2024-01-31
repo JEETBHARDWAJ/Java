e6b1af4168e992bc5ddd8695d8907785a4268234
JEETBHADWAJ <bhardwajjeet408@gmail.com>
2024-02-01 01:04:33 +0530

    abstaract

--- Abstractt.java
+++ Abstractt.java
@@ -1,24 +1,44 @@
 // abs class can not  access by object
-abstract class Abs{
+abstract class Bank{
 
-    int a,b ;
-    void fun(int p ,int q){
-        a = p;
-        b = q;
+    String holder_name;
+    int mob_no; 
+    String addres;
+    String date_of_birth;
+    void insert_detail(String s1, int i1, String s2, String s3)
+    {
+        holder_name = s1;
+        mob_no = i1;
+        addres = s2;
+        date_of_birth=s3;
     }
 
+    void user_detail(){
+        System.out.println(holder_name);
+        System.out.println(mob_no);
+        System.out.println(addres);
+        System.out.println(date_of_birth);
+    }
+    
 }
-class Trac  extends Abs{
 
+class  Saving extends Bank{
+   Saving() {System.out.println("saving");}
+    
+    
+}
+class Current extends Bank{
+Current() {System.out.println("Current");}
 
 }
+
 
 class Abstractt{
 
     public static void main(String args[])
     {
-        Trac t1 = new Trac();
-        System.out.println(t1.a);
+       Saving s1 = new Saving();
+       s1.insert_detail("jeet", 1221," asf", "fal");
         
     }
 }
--- Array/Mattrixs.java
+++ Array/Mattrixs.java
@@ -1,21 +1,22 @@
 
-public class Multip_matrix {
+
+public class Mattrixs {
 
     public static void main(String []args)
     {
         int matrixm1[][] = {{1,3,1},{2,1,1}}; 
-        int matrixm2[][] = {{2,1,1},{3,2,1}}; 
-        int sum_matrix[][] = new int[2][2]; 
+        int matrixm2[][] = {{2,1,1},{3,2,1},{1,23,3}}; 
+        int sum_matrix[][] = new int[3][3]; 
         int sum = 0;
 
 
-        for(int x = 0; x<3;x++)
+        for(int x = 0; x<2;x++)
         {
             for(int y = 0; y<3;y++){
-                for(int z = 0 ; z<2;z++){
+                for(int z = 0 ; z<3;z++){
                     sum = sum + matrixm1[x][z]*matrixm2[z][y];
-                    sum_matrix[x][y] = sum;
                 }
+                sum_matrix[x][y] = sum;
             }
         }
 
@@ -23,9 +24,9 @@ public class Multip_matrix {
 
 
         //display sum of the matrix
-        for(int i = 0; i<2;i++)
+        for(int i = 0; i<3;i++)
         {
-            for(int j = 0 ; j<2; j++)
+            for(int j = 0 ; j<3; j++)
             {
                 System.out.println(sum_matrix[i][j]);
             }
