278cfc11a440b92975d3789b58071d9b253557b5
JEETBHADWAJ <bhardwajjeet408@gmail.com>
2024-02-21 08:36:28 +0530

    ford add

---
+++ Part 1  8 houre Complited/java/ford.java
@@ -0,0 +1,12 @@
+/**
+ * ford
+ */
+import package1.*;
+public class ford {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.detail();
+    }
+
+    
+}
---
+++ Part 1  8 houre Complited/java/package1/Car.java
@@ -0,0 +1,15 @@
+package package1;
+public class Car {
+    String brand = "Ford"; 
+    int price = 40000000;
+    String color = "z-Black";
+    String carnum = "BR01FP0403";
+
+    public void detail() { 
+        System.out.println(brand);
+        System.out.println(price);
+        System.out.println(color);
+
+    }
+
+}
