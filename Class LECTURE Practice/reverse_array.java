60ba59c63912361432fd2d502eb01a210d00894c
JEETBHADWAJ <bhardwajjeet408@gmail.com>
2024-02-24 22:08:01 +0530

    hello new change

--- Class LECTURE Practice/reverse_array.java
+++ Class LECTURE Practice/reverse_array.java
@@ -1,8 +1,8 @@
 public class reverse_array {
     public static void main(String[] args) {
-        int array1 = []
+        int array1 = [ASKFNSKNDFK
         for(){
-            
+
         }
     }
     
